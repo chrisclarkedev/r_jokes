ec25853c8c2f40a525af9aaecc863bcef24c8fa9
chrisclarkedev <cclarke8904@gmail.com>
2021-11-12 14:49:41 -0500

    created function that fetches random joke from api url, set joke and new joke into state, set onclick event

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 const API_URL = `http://api.icndb.com/jokes/random`;
 
 function App() {
+  const [joke, setJoke] = useState('');
+
+  const generateJoke = () => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setJoke(data.value.joke));
+  };
+
+  useEffect(() => {
+    generateJoke();
+  }, []);
+
   return (
     <div class="box">
       <h3>Random Jokes Generator</h3>
-      <p>Joke is here</p>
-      <button>Get new joke</button>
+      <p>{joke}</p>
+      <button onClick={generateJoke}>Get new joke</button>
     </div>
   );
 }
