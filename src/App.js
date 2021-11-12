001d3d76d4b4388972652bd0e8d8909246e11a30
chrisclarkedev <cclarke8904@gmail.com>
2021-11-12 14:25:23 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+const API_URL = `http://api.icndb.com/jokes/random`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="box">
+      <h3>Random Jokes Generator</h3>
+      <p>Joke is here</p>
+      <button>Get new joke</button>
     </div>
   );
 }
